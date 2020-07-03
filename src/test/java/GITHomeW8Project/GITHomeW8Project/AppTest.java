e1a15a8dcac9cbc271fcbd5abc72c7eca70df02d
Marix2020 <maricelocampo888@gmail.com>
2020-07-03 17:59:27 -0400

    HW Practice8

---
+++ src/test/java/GITHomeW8Project/GITHomeW8Project/AppTest.java
@@ -0,0 +1,20 @@
+package GITHomeW8Project.GITHomeW8Project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
